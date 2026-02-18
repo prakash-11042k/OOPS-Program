6c7c75b8356403ddaedcb5d5fc790df8ddb3b15e
prakash <prakash11042k@gmail.com>
2026-02-18 10:54:10 +0530

    OOPS Program

---
+++ Car.java
@@ -0,0 +1,16 @@
+package Object;
+
+public class Car {
+
+	String name;
+	double price;
+	String color;
+	static String owner="Hema";
+	
+	public void run() {
+		System.out.println(name+"is Running");
+	}
+	public void stop() {
+		System.out.println(name+"Stops");
+	}
+}
---
+++ CarObjGen.java
@@ -0,0 +1,15 @@
+package Object;
+
+public class CarObjGen {
+
+	public static void main(String[] args) {
+		
+		Car c1=new Car();
+		Car c2=new Car();
+		Car c3=new Car();
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+	}
+}
