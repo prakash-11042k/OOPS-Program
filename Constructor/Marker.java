9650d4c583f0c650acc79c246c2ae318383fce20
prakash <prakash11042k@gmail.com>
2026-03-03 18:42:17 +0530

    Constructor Example

---
+++ Constructor/Marker.java
@@ -0,0 +1,15 @@
+package Object;
+
+public class Marker {
+
+	String color="Blue";
+	
+	public static void main(String[] args) {
+		
+		Marker m1=new Marker();
+		Marker m2=new Marker();
+		
+		System.out.println(m1.color);
+		System.out.println(m2.color);
+	}
+}
